3f7aa8523d5e2423eef025b6662e679db5371ccf
Fabio <fabiodn.798@gmail.com>
2023-01-30 08:39:29 +0100

    Modify descriptions to testing, upgrade readme, test  sonar

--- JS/functions.test.js
+++ JS/functions.test.js
@@ -6,7 +6,7 @@ import {
 } from './functions.js';
 import { arr, rows } from './matris.js';
 
-describe('Game of Life Functions', () => {
+
   // BeforeEach(() => {
   //   jest.useFakeTimers();
   // });
@@ -16,62 +16,78 @@ describe('Game of Life Functions', () => {
   // })
 
   // Test createMatrix function
-  test('createMatrix function creates a matrix with the specified number of rows', () => {
-    const matrix = createMatrix(arr, rows);
+  describe('given an array', () => {
+    describe('when createMatrix is called', () =>{
+      test('creates a matrix with the specified number of rows', () => {
+        const matrix = createMatrix(arr, rows);
+        expect(matrix.length).toBe(rows);
+        });
+      });
+    });
 
-    expect(matrix.length).toBe(rows);
-    expect(matrix[0].length).toBe(rows);
-  });
+
 
   // Test countSurroundings function
-  test('countSurroundings function counts the number of surrounding elements with value 1', () => {
-    const matrix = [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 1, 0],
-    ];
-    const i = 1;
-    const j = 1;
+  describe('given an array', () => {
+    describe('when createMatrix is called', () =>{
+      test('countSurroundings function counts the number of surrounding elements with value 1', () => {
+            const matrix = [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 1, 0],
+        ];
+        const i = 1;
+        const j = 1;
 
-    expect(countSurroundings(matrix, i, j)).toBe(4);
+        expect(countSurroundings(matrix, i, j)).toBe(4);
+      });
+    });
   });
 
   // Test generateNextIteration function
-  test('generateNextIteration function generates the next iteration of the matrix', () => {
-    const matrix = [
-      [0, 1, 0],
-      [1, 0, 1],
-      [0, 0, 0],
-    ];
-    const nextMatrix = generateNextIteration(matrix);
+  describe('given an array', () => {
+    describe('when createMatrix is called', () =>{
+      test('generateNextIteration function generates the next iteration of the matrix', () => {
+        const matrix = [
+          [0, 1, 0],
+          [1, 0, 1],
+          [0, 0, 0],
+        ];
+        const nextMatrix = generateNextIteration(matrix);
 
-    expect(nextMatrix).not.toBe(matrix);
-    expect(nextMatrix[0][1]).toBe(1);
-    expect(nextMatrix[1][0]).toBe(0);
+        expect(nextMatrix).not.toBe(matrix);
+        expect(nextMatrix[0][1]).toBe(1);
+        expect(nextMatrix[1][0]).toBe(0);
+      });
+    });
   });
 
   // Test matricesAreEqual function
-  test('matricesAreEqual function returns true if matrices are equal, false otherwise', () => {
-    const matrix1 = [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 1, 0],
-    ];
-    const matrix2 = [
-      [0, 1, 0],
-      [1, 1, 1],
-      [0, 1, 0],
-    ];
-    const matrix3 = [
-      [0, 1, 0],
-      [1, 0, 1],
-      [0, 1, 0],
-    ];
+describe('given an array', () => {
+    describe('when createMatrix is called', () =>{
+      test('matricesAreEqual function returns true if matrices are equal, false otherwise', () => {
+        const matrix1 = [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 1, 0],
+        ];
+        const matrix2 = [
+          [0, 1, 0],
+          [1, 1, 1],
+          [0, 1, 0],
+        ];
+        const matrix3 = [
+          [0, 1, 0],
+          [1, 0, 1],
+          [0, 1, 0],
+        ];
 
-    expect(matricesAreEqual(matrix1, matrix2)).toBe(true);
-    expect(matricesAreEqual(matrix1, matrix3)).toBe(false);
+        expect(matricesAreEqual(matrix1, matrix2)).toBe(true);
+        expect(matricesAreEqual(matrix1, matrix3)).toBe(false);
+      });
+    });
   });
-});
+
 
 // Jest.useFakeTimers();
 
